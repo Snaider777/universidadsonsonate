da31e0860e56e8332c8ef2bd6a4a00ee516bca2d
Carlos Francisco Chavarria Hernandez <knightchaper777@gmail.com>
2025-02-19 22:48:03 -0600

    CommitRamita

--- app/rutita/page.js
+++ app/rutita/page.js
@@ -2,6 +2,14 @@ export default function Home() {
   return (
     <div >
       <h1>Hola soy la rama ramita</h1>
+      <h1>Hola soy la rama ramita</h1>
+      <h1>Hola soy la rama ramita</h1>
+      <h1>Hola soy la rama ramita</h1>
+      <h1>Hola soy la rama ramita</h1>
+      <h1>Hola soy la rama ramita</h1>
+      <h1>Hola soy la rama ramita</h1>
+      <h1>Hola soy la rama ramita</h1>
+      <h1>Hola soy la rama ramita</h1>
     </div>
   );
 }
