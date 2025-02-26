e4643e91583f40ddee44f6fc06ff1e97ccc50a01
Snaider777 <cristophersnayder0@gmail.com>
2025-02-26 09:52:31 -0600

    Edicion de la bienvenida USO

--- Components/Locale/index/BienvenidaUSO.js
+++ Components/Locale/index/BienvenidaUSO.js
@@ -34,34 +34,39 @@ const BienvenidaUSO = () => {
     hidden: { opacity: 0, x: -90 },
     visible: { opacity: 1, x: 0 }
   };
-
+  const divAnimation = {
+    hidden: { opacity: 0, x: 90 },
+    visible: { opacity: 1, x: 0 }
+  };
+  const figuraAnition={
+    hidden: {opacity: 0, y: -100},
+    visible: {opacity: 1, x: 0}
+  };
   const imgAnimation = {
     hidden: { opacity: 0, x: 90 },
     visible: { opacity: 1, x: 0 }
   };
   const config = {
-    dots: true,
+    
     infinite: true,
     autoplay: true,
     autoplaySpeed: 5000,
     speed: 800,
     fade: true,
-    arrows: false,
+    arrows: true,
     pauseOnHover: false,
 };
   return (
-    <div ref={ref} className="overflow-hidden bg-blue-500">
+    <div ref={ref} className="overflow-hidden bg-[#0f8fce]">
       {/* Sección con fondo mejorado */}
       <motion.div
         initial={{ opacity: 0, scale: 0.95 }}
-        animate={
-          inView ? { opacity: 1, scale: 1 } : { opacity: 0, scale: 1 }
-        }
+        animate={inView ? {opacity: 1, x: 0 } : {}}
         transition={{ duration: 0.8 }}
-        className="relative flex flex-col md:flex-col lg:flex-row lg:w-screen justify-center items-center h-auto w-full p-4 bg-blue-500"
+        className="relative flex flex-col md:flex-col lg:flex-row lg:w-screen justify-center items-center h-auto w-full p-4 bg-[#0f8fce]"
       >
         {/* Contenido de texto */}
-        <motion.div
+          <motion.div
           className="relative flex flex-col justify-center items-start p-4 md:p-8 w-full md:w-full lg:w-1/2 text-white"
           initial="hidden"
           animate={inView ? 'visible' : 'hidden'}
@@ -86,7 +91,7 @@ const BienvenidaUSO = () => {
             nuestra universidad!
           </p>
           <motion.button
-            className="bg-green-500 text-white font-semibold text-sm md:text-base lg:text-lg px-4 py-2 rounded-lg mt-2 hover:bg-green-600"
+            className="bg-[#05ce0e] text-white font-semibold text-sm md:text-base lg:text-lg px-4 py-2 rounded-lg mt-2 hover:bg-green-600"
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.95 }}
           >
@@ -96,50 +101,58 @@ const BienvenidaUSO = () => {
           </motion.button>
         </motion.div>
         <Slider 
+          key={slides.id}
           className="relative flex items-center justify-center w-full lg:w-1/2 h-auto mt-8 lg:mr-10"
           {...config}>
           {slides.map((slide, index) => (
-          <div key={index}>
-              <div className=" h-auto md:h-auto w-full">
-                {/* Imagen de fondo */}
-                  <img
-                      src={slide.imagen}
-                      alt={slide.titulo}
-                      className="w-full h-full object-cover"
-                  />
-                {/* Capa de superposición */}
-                  <div className="absolute inset-0 bg-black opacity-30"></div>
-                {/* Contenido del slide */}
-                  <div className="  absolute inset-0 flex flex-col justify-center items-start max-w-2xl mx-auto px-6 md:px-12">
-                  <motion.h2
-                      className=" text-3xl md:text-5xl font-bold text-white mb-4"
-                      initial={{ opacity: 0, y: 20 }}
-                      animate={inView ? { opacity: 1, y: 0 } : {}}
-                      transition={{ duration: 0.8, delay: 0.2 }}
-                  >
-                      {slide.titulo}
-                  </motion.h2>
-                  <motion.p
-                      className="text-lg md:text-2xl text-white mb-6"
-                      initial={{ opacity: 0, y: 20 }}
-                      animate={inView ? { opacity: 1, y: 0 } : {}}
-                      transition={{ duration: 0.8, delay: 0.2 }}
-                  >
-                      {slide.descripcion}
-                  </motion.p>
-                  <motion.button
-                      className="bg-green-500 text-white px-6 py-3 rounded-lg font-semibold hover:bg-green-600"
-                      initial={{ opacity: 0, y: 20 }}
-                      animate={inView ? { opacity: 1, y: 0 } : {}}
-                      transition={{ duration: 0.8, delay: 0.4 }}
-                      whileTap={{ scale: 0.95 }}
-                      whileHover={{ scale: 1.05 }}
-                  >
-                      Descubre más
-                  </motion.button>
+          <motion.div
+            variants={divAnimation}
+            initial='hidden'
+            animate={inView ? 'visible' : 'hidden'}
+            transition={{duration: 1}}
+          >
+            <div key={index}>
+                <div className=" h-auto md:h-auto w-full">
+                  {/* Imagen de fondo */}
+                    <img
+                        src={slide.imagen}
+                        alt={slide.titulo}
+                        className="w-full h-full object-cover"
+                    />
+                  {/* Capa de superposición */}
+                  <div className="absolute inset-0 bg-black opacity-30 mb-1.5"></div>
+                  {/* Contenido del slide */}
+                    <div className="  absolute inset-0 flex flex-col justify-center items-start max-w-2xl mx-auto px-6 md:px-12">
+                    <motion.h2
+                        className=" text-3xl md:text-5xl font-bold text-white mb-4"
+                        initial={{ opacity: 0, y: 20 }}
+                        animate={inView ? { opacity: 1, y: 0 } : {}}
+                        transition={{ duration: 0.8, delay: 0.2 }}
+                    >
+                        {slide.titulo}
+                    </motion.h2>
+                    <motion.p
+                        className="text-lg md:text-2xl text-white mb-6"
+                        initial={{ opacity: 0, y: 20 }}
+                        animate={inView ? { opacity: 1, y: 0 } : {}}
+                        transition={{ duration: 0.8, delay: 0.2 }}
+                    >
+                        {slide.descripcion}
+                    </motion.p>
+                    <motion.button
+                        className="bg-[#05ce0e] text-white px-6 py-3 rounded-lg font-semibold hover:bg-green-600"
+                        initial={{ opacity: 0, y: 20 }}
+                        animate={inView ? { opacity: 1, y: 0 } : {}}
+                        transition={{ duration: 0.8, delay: 0.4 }}
+                        whileTap={{ scale: 0.95 }}
+                        whileHover={{ scale: 1.05 }}
+                    >
+                        Descubre más
+                    </motion.button>
+                </div>
               </div>
             </div>
-          </div>
+          </motion.div>
         ))}
         </Slider>
         {/* Sección de imágenes 
@@ -166,6 +179,17 @@ const BienvenidaUSO = () => {
             </div>
           </div>
         </motion.div>*/}
+      </motion.div>
+      <motion.div
+          initial={{opacity: 0, y: 40}}
+          animate={inView ? 'visible' : 'hidden'}
+          transition={{duration: 0.9}}
+          variants={figuraAnition}
+      >
+        <div className="relative w-full h-16">
+              <div className="absolute top-0 left-0 w-full h-20 bg-[#05ce0e] transform -skew-y-2 origin-top-left"
+              ></div>
+        </div>
       </motion.div>
     </div>
   );
