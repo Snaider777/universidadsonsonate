2ff19cd7fad175c0df91068266863ac6cfd4a3e9
Snaider777 <cristophersnayder0@gmail.com>
2025-05-17 22:48:04 -0600

    Actualización de la lista de facultades y programas académicos en SlideOfertaAcademica.js

--- Components/Locale/index/SlideOfertaAcademica.js
+++ Components/Locale/index/SlideOfertaAcademica.js
@@ -7,30 +7,149 @@ const facultades = [
   {
     nombre: "Ingeniería en Sistemas",
     descripcion: "Forma parte del futuro tecnológico con nuestra carrera de Ingeniería en Sistemas.",
-    imagen: "/images/ingenieriasistemas.jpg",
-    categoria: "Ingeniería en Sistemas",
+    imagen: "/images/img-inicio-2.png",
+    categoria: "Facultad de Ingenieria y Ciencias Naturales",
     href: "/oferta/ingenieria-sistemas"
   },
   {
-    nombre: "Administración de Empresas",
+    nombre: "Ingeniería Electrica",
+    descripcion: "Forma parte del futuro tecnológico con nuestra carrera de Ingeniería en Sistemas.",
+    imagen: "/images/img-inicio-2.png",
+    categoria: "Facultad de Ingenieria y Ciencias Naturales",
+    href: "/oferta/ingenieria-sistemas"
+  },
+  {
+    nombre: "Ingeniería en Agronegocios",
+    descripcion: "Forma parte del futuro tecnológico con nuestra carrera de Ingeniería en Sistemas.",
+    imagen: "/images/img-inicio-2.png",
+    categoria: "Facultad de Ingenieria y Ciencias Naturales",
+    href: "/oferta/ingenieria-sistemas"
+  },
+  {
+    nombre: "Ingeniería Industrial",
+    descripcion: "Forma parte del futuro tecnológico con nuestra carrera de Ingeniería en Sistemas.",
+    imagen: "/images/img-inicio-2.png",
+    categoria: "Facultad de Ingenieria y Ciencias Naturales",
+    href: "/oferta/ingenieria-sistemas"
+  },
+  {
+    nombre: "Licenciatura Administración de Empresas",
     descripcion: "Desarrolla tus habilidades en gestión y liderazgo empresarial.",
     imagen: "/images/img-inicio-2.png",
-    categoria: "Administración de Empresas",
+    categoria: "Facultad de economia y ciencia sociales",
     href: "/oferta/administracion-empresas"
   },
   {
-    nombre: "Derecho",
+    nombre: "Licenciatura en Contabilidad",
+    descripcion: "Desarrolla tus habilidades en gestión y liderazgo empresarial.",
+    imagen: "/images/img-inicio-2.png",
+    categoria: "Facultad de economia y ciencia sociales",
+    href: "/oferta/administracion-empresas"
+  },
+  {
+    nombre: "Licenciatura en Psicologia",
+    descripcion: "Desarrolla tus habilidades en gestión y liderazgo empresarial.",
+    imagen: "/images/img-inicio-2.png",
+    categoria: "Facultad de economia y ciencia sociales",
+    href: "/oferta/administracion-empresas"
+  },
+  {
+    nombre: "Licenciatura en Mercadeo",
+    descripcion: "Desarrolla tus habilidades en gestión y liderazgo empresarial.",
+    imagen: "/images/img-inicio-2.png",
+    categoria: "Facultad de economia y ciencia sociales",
+    href: "/oferta/administracion-empresas"
+  },
+  {
+    nombre: "Licenciatura en Ciencias politicas",
     descripcion: "Contribuye a la justicia y al bienestar social estudiando Derecho.",
-    imagen: "/images/juridica.webp",
-    categoria: "Derecho",
+    imagen: "/images/psicologia.webp",
+    categoria: "Facultad de Ciencias Juridicas",
     href: "/oferta/derecho"
   },
   {
-    nombre: "Psicología",
+    nombre: "Licenciatura en A¿Matematica",
     descripcion: "Comprende el comportamiento humano y ayuda a los demás con Psicología.",
     imagen: "/images/psicologia.webp",
-    categoria: "Psicología",
+    categoria: "Escuela de Educacion",
     href: "/oferta/psicologia"
+  },
+  {
+    nombre: "Licenciatura en Lenguaje y Literatura",
+    descripcion: "Comprende el comportamiento humano y ayuda a los demás con Psicología.",
+    imagen: "/images/psicologia.webp",
+    categoria: "Escuela de Educacion",
+    href: "/oferta/psicologia"
+  },
+  {
+    nombre: "Licenciatura en Ciencias Naturales",
+    descripcion: "Comprende el comportamiento humano y ayuda a los demás con Psicología.",
+    imagen: "/images/psicologia.webp",
+    categoria: "Escuela de Educacion",
+    href: "/oferta/psicologia"
+  },
+  {
+    nombre: "Licenciatura en Educación Física, Deportes y Recreación",
+    descripcion: "Comprende el comportamiento humano y ayuda a los demás con Psicología.",
+    imagen: "/images/psicologia.webp",
+    categoria: "Escuela de Educacion",
+    href: "/oferta/psicologia"
+  },
+  {
+    nombre: "Maestría en Neuropsicología del Aprendizaje",
+    descripcion: "Diseña y construye el futuro con nuestra carrera de Arquitectura.",
+    imagen: "/images/img-inicio-2.png",
+    categoria: "Maestrias",
+    href: "/oferta/arquitectura"
+  },
+  {
+    nombre: "Maestría en Atención Integral de la Primera Infancia",
+    descripcion: "Diseña y construye el futuro con nuestra carrera de Arquitectura.",
+    imagen: "/images/img-inicio-2.png",
+    categoria: "Maestrias",
+    href: "/oferta/arquitectura"
+  },
+  {
+    nombre: "Maestría en Innovación Educativa",
+    descripcion: "Diseña y construye el futuro con nuestra carrera de Arquitectura.",
+    imagen: "/images/img-inicio-2.png",
+    categoria: "Maestrias",
+    href: "/oferta/arquitectura"
+  },
+  {
+    nombre: "Maestría en Administración Financiera",
+    descripcion: "Diseña y construye el futuro con nuestra carrera de Arquitectura.",
+    imagen: "/images/img-inicio-2.png",
+    categoria: "Maestrias",
+    href: "/oferta/arquitectura"
+  },
+  {
+    nombre: "Maestría en Gerencia de Proyectos",
+    descripcion: "Diseña y construye el futuro con nuestra carrera de Arquitectura.",
+    imagen: "/images/img-inicio-2.png",
+    categoria: "Maestrias",
+    href: "/oferta/arquitectura"
+  },
+  {
+    nombre: "Técnico en Logística y Aduanas",
+    descripcion: "Domina las finanzas y la gestión contable con nuestra carrera de Contabilidad.",
+    imagen: "/images/img-inicio-2.png",
+    categoria: "Tecnicos",
+    href: "/oferta/contabilidad"
+  },
+  {
+    nombre: "Técnico en Mantenimiento Informático",
+    descripcion: "Domina las finanzas y la gestión contable con nuestra carrera de Contabilidad.",
+    imagen: "/images/img-inicio-2.png",
+    categoria: "Tecnicos",
+    href: "/oferta/contabilidad"
+  },
+  {
+    nombre: "Técnico Superior en Ciencia de Datos",
+    descripcion: "Domina las finanzas y la gestión contable con nuestra carrera de Contabilidad.",
+    imagen: "/images/img-inicio-2.png",
+    categoria: "Tecnicos",
+    href: "/oferta/contabilidad"
   }
 ];
 
