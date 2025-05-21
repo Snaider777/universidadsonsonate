29614381f63289ccb5fc05b40f05c6e68b3f93d9
Eduardo Iraheta <eduardoiraheta87@gmail.com>
2025-05-21 11:40:00 -0600

    sisi

---
+++ app/ofertaAcademica/fics/page.js
@@ -0,0 +1,12 @@
+import InicioOferta from "@/Components/Locale/ofertaAcademica/InicioOferta"
+import MisionVisionHistoriaSection from "@/Components/Locale/nuestraInstitucion/MisionVisionHistoriaSection"
+import Hero from "@/Components/Locale/index/Hero"
+export default function NuestraIntitucion(){
+    return(
+        <>
+            <Hero/>
+            <InicioOferta/>
+            <MisionVisionHistoriaSection/>
+        </>
+    )
+}
